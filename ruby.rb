204672603eacc3728a969e472d5a4a6b5eef324f
a5hk <5412540+a5hk@users.noreply.github.com>
2020-04-23 17:18:11 +0430

    ruby

---
+++ ruby.rb
@@ -0,0 +1,35 @@
+require 'net/http'
+
+module ExampleModule
+  class ExampleClass < ParentClass
+    def initialize(cv, iv)
+      super
+      @@classvar = cv
+      @instancevar = iv
+    end
+
+    def method1()
+      self.p_method()
+    end
+
+    def self.method2()
+      if @@classvar <= 10
+        puts "<= 10"
+      end
+    end
+
+    private
+
+    def p_method()
+      x = {a: 5, b: 6}
+
+      x.each do |k, v|
+        puts "Key: #{k}, Value: #{v}"
+      end
+    end
+  end
+end
+
+ec = ExampleClass.new(1, 2)
+ec.method1()
+ExampleClass.method2()
